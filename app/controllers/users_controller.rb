4c577e9ff4beb201f82da28615cb9daa572e185f
railsbeginner <bartosz.dyja@gmail.com>
2016-02-03 00:33:44 +0100

    Refactor to slim and add flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, flash: { error: "User saved successfully!!" }
+      redirect_to @user, flash: { success: "User saved successfully!!" }
     else
       flash[:error] = 'Error'
       render 'new'
