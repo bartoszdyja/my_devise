163d11444deca3f8d17765d24960ade636a8565c
railsbeginner <bartosz.dyja@gmail.com>
2016-02-03 14:00:54 +0100

    Add session helpers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
       redirect_to user, flash: { notice: "User logged in successfully" }
     else
       flash.now[:error] = 'Wrong username / password.'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user, except: [:new]
   def new
     @user = User.new
   end
